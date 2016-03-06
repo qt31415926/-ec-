1e2806fe27521cf854ddb07fb6685d2bb2764d33
Robin Pham <robin.pham@gmail.com>
2016-03-05 20:18:52 -0500

    Added: npm build script, webpack production env variable

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -16,6 +16,9 @@ module.exports = {
         warnings: false,
         screw_ie8: true
       }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ],
   module: {
