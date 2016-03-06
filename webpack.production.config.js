583138a302d3044c2e22864da6de9922cbd6c830
Robin Pham <robin.pham@gmail.com>
2016-03-05 20:24:22 -0500

    Webpack plugin production var

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -18,7 +18,7 @@ module.exports = {
       }
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify('production')
     })
   ],
   module: {
